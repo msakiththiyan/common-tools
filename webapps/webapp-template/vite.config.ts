60ce7f564c7183ad00031c1ac77bc5f4efed3c77
dinethde <dinethran33@gmail.com>
2025-11-17 18:26:05 +0530

    fix: add react plugin to Vite configuration

--- webapps/webapp-template/vite.config.ts
+++ webapps/webapp-template/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     port: 3000, // Change this to your desired port number
   },
